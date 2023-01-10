875ca4a325db15787acbba8befdd2327cb008b2e
Daniel <daniel.bailey.dev@gmail.com>
2023-01-10 17:59:28 +1100

    added config folder to help seperate code

--- server.js
+++ server.js
@@ -4,19 +4,8 @@ const app = express();
 const indexRoutes = require('./Routes/index');
 const productsRoutes = require('./Routes/products');
 const adminRoutes = require('./Routes/admin');
-const mongoose = require('mongoose');
+const connectDB = require('./config/database')
 
-// Connect DB
-async function connectDB() {
-  try {
-    mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
-}
 connectDB();
 
 app.set('view engine', 'ejs');
