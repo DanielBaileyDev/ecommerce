8bbac66b588a08d1a74f3e749bca0b6dc8e8ba36
Daniel <daniel.bailey.dev@gmail.com>
2023-01-31 18:34:47 +1100

    Added search bar on products page and admin page

--- public/js/main.js
+++ public/js/main.js
@@ -33,3 +33,20 @@ window.onclick = function (event) {
     }
   }
 }
+
+function searchProducts(){
+  var input, filter, txtValue;
+  input = document.getElementById('search-input');
+  filter = input.value.toUpperCase();
+  products = document.getElementsByClassName("product");
+
+  for(let i = 0; i < products.length; i++){
+    let productName = products[i].getElementsByClassName('product-name')[0];
+    txtValue = productName.textContent || productName.innerText || productName.value;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      products[i].style.display = "";
+    } else {
+      products[i].style.display = "none";
+    }
+  }
+}
